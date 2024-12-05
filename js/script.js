d249137789b5210a4a3766d4d2337a2036906bee
beyxyy <antho.kalbe@gmail.com>
2024-12-05 22:30:22 +0100

    Implement slide functionality with question display and answer checking; add new HTML and CSS files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
+let slideIndex = 0;
+
+const updateIndex = () => {
+  slideIndex++;
+}
+
+const getDameTextContainer = (slideIndex) => {
+  let slide = document.querySelector('.active .dame_question');
+  let slideText = slide.textContent;
+  return slideText;
+}
+
+const displayText = (text) => {
+  document.querySelector('.active .phrase_dame').textContent = text;
+}
+
+//Set up the text for the given slide index
+const displayQuestion = (slideIndex) => {
+  let slideText = getDameTextContainer(slideIndex);
+  slideText.textContent = text[slideIndex].phrase_dame_1;
+  text[slideIndex].answers.forEach((answer, index) => {
+    document.querySelector('#popup_question label#label_q'+index).textContent = answer;
+    document.querySelector('#popup_question input[type="radio"]#q'+index).value = index;
+  });
+}
+
+
+const checkAnswer = (slideIndex) => {
+  const answer = document.querySelector('popup_question input[type="radio"]:checked').value;
+  const correctAnswer = text[slideIndex].correct;
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+  } else {
+    console.log('Incorrect!');
+  }
+  
+}
+
+
 
---
+++ js/tests.js
@@ -0,0 +1,24 @@
+let text=  {
+    "slide1": {
+      "phrase_dame": "La phrase de la dame",
+      "answers": [
+        "Réponse 1",
+        "Réponse 2",
+        "Réponse 3",
+        "Réponse 4"
+      ],
+      "correct": 1,
+      "phrase_dame_2": "La phrase de la dame 2"
+    },
+    "slide2": {
+      "phrase_dame": "La phrase de la dame",
+      "answers": [
+        "Réponse 1",
+        "Réponse 2",
+        "Réponse 3",
+        "Réponse 4"
+      ],
+      "correct": 2,
+      "phrase_dame_2": "La phrase de la dame 2"
+    },
+  }
